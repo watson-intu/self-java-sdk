635fc29b81a53b46d5dd381ed0b74e61727344fb
John Andersen <jjanders@us.ibm.com>
2016-11-02 17:33:02 -0500

    AgentSociety implementation

--- self-java-sdk/src/main/java/com/ibm/watson/self/agents/AgentSociety.java
+++ self-java-sdk/src/main/java/com/ibm/watson/self/agents/AgentSociety.java
@@ -1,5 +1,100 @@
 package com.ibm.watson.self.agents;
 
-public class AgentSociety {
+import java.util.HashMap;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.ibm.watson.self.topics.IEvent;
+import com.ibm.watson.self.topics.TopicClient;
+
+public class AgentSociety implements IEvent {
+
+	public static AgentSociety instance = null;
+	private boolean started = false;
+	
+	private HashMap<String, IAgent> agentMap = new HashMap<String, IAgent>();
+	private HashMap<String, Boolean> overrideMap = new HashMap<String, Boolean>();
+	
+	public AgentSociety() {
+		TopicClient.getInstance().subscribe("agent-society", this);
+		started = true;
+	}
+	
+	public static AgentSociety getInstance() {
+		if(instance == null) {
+			instance = new AgentSociety();
+		}
+		
+		return instance;
+	}
+	
+	public boolean isRegistered(IAgent agent) {
+		return agentMap.containsKey(agent.getAgentId());
+	}
+	
+	public void addAgent(IAgent agent, boolean override) {
+		if(!agentMap.containsKey(agent.getAgentId())) {
+			JsonObject wrapperObject = new JsonObject();
+			wrapperObject.addProperty("event", "add_agent_proxy");
+			wrapperObject.addProperty("agentId", agent.getAgentId());
+			wrapperObject.addProperty("name", agent.getAgentName());
+			wrapperObject.addProperty("override", override);
+			TopicClient.getInstance().publish("agent-society", wrapperObject.toString(), false);
+			agentMap.put(agent.getAgentId(), agent);
+			overrideMap.put(agent.getAgentId(), override);
+		}
+	}
+	
+	public void removeAgent(IAgent agent) {
+		if(agentMap.containsKey(agent.getAgentId())) {
+			agentMap.remove(agent.getAgentId());
+			overrideMap.remove(agent.getAgentId());
+			JsonObject wrapperObject = new JsonObject();
+			wrapperObject.addProperty("event", "remove_agent_proxy");
+			wrapperObject.addProperty("agentId", agent.getAgentName());
+			TopicClient.getInstance().publish("agent-society", wrapperObject.toString(), false);
+		}
+	}
+
+	public void onEvent(String event) {
+		JsonParser parser = new JsonParser();
+		JsonObject wrapperObject = parser.parse(event).getAsJsonObject();
+		String agentId = wrapperObject.get("agentId").getAsString();
+		IAgent agent = agentMap.get(agentId);
+		if(agent == null) {
+			System.out.println("Failed to find agent!");
+			return;
+		}
+		
+		boolean failed = false;
+		String eventName = wrapperObject.get("event").getAsString();
+		if(eventName.equals("start_agent")) {
+			if(!agent.onStart()) {
+				System.out.println("Failed to start agent: " + agent.getAgentName());
+				failed = true;
+			}
+		}
+		else if(eventName.equals("stop_agent")) {
+			if(!agent.onStop()) {
+				System.out.println("Failed to stop agent: " + agent.getAgentName());
+				failed = true;
+			}
+		}
+		
+		if(failed) {
+			JsonObject failedObject = new JsonObject();
+			failedObject.addProperty("failed_event", eventName);
+			failedObject.addProperty("event", "error");
+			TopicClient.getInstance().publish("agent-society", wrapperObject.toString(), false);
+		}
+	}
+
+	public boolean isActive() {
+		return started;
+	}
+
+	public void shutdown() {
+		started = false;
+		TopicClient.getInstance().unsubscribe("agent-society", this);
+	}
 }
--- self-java-sdk/src/main/java/com/ibm/watson/self/agents/IAgent.java
+++ self-java-sdk/src/main/java/com/ibm/watson/self/agents/IAgent.java
@@ -2,4 +2,27 @@ package com.ibm.watson.self.agents;
 
 public interface IAgent {
 
+	/**
+	 * This should return a text name for this agent
+	 * @return
+	 */
+	public String getAgentName();
+	
+	/**
+	 * This should return a unique id for the agent
+	 * @return - unique id
+	 */
+	public String getAgentId();
+	
+	/**
+	 * This is invoked when this agent should start
+	 * @return - if it started
+	 */
+	public boolean onStart();
+	
+	/**
+	 * This is invoked when the agent should stop
+	 * @return - if it stopped
+	 */
+	public boolean onStop();  
 }
