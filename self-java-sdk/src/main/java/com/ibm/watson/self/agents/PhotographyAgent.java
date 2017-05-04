36a4cf889824256ab6be3302060ef40ef0a49bdc
Erika Dsouza <Erika.Dsouza@ibm.com>
2017-05-04 11:02:47 -0500

    INTU-1363 | Added IBM headers

--- self-java-sdk/src/main/java/com/ibm/watson/self/agents/PhotographyAgent.java
+++ self-java-sdk/src/main/java/com/ibm/watson/self/agents/PhotographyAgent.java
@@ -1,3 +1,20 @@
+/**
+* Copyright 2016 IBM Corp. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
 package com.ibm.watson.self.agents;
 
 import java.util.UUID;
@@ -10,10 +27,9 @@ import com.google.gson.JsonParser;
 import com.ibm.watson.self.blackboard.BlackBoard;
 import com.ibm.watson.self.blackboard.IBlackBoard;
 import com.ibm.watson.self.blackboard.IThing;
-import com.ibm.watson.self.blackboard.ThingEvent;
 import com.ibm.watson.self.blackboard.IThing.ThingEventType;
+import com.ibm.watson.self.blackboard.ThingEvent;
 import com.ibm.watson.self.sensors.ILocalSensorSubscriber;
-import com.ibm.watson.self.sensors.SensorManager;
 
 /**
  * This agent takes a snapshot of a person on photography_intent. In case it cannot see the person,
--- self-java-sdk/src/test/java/com/ibm/watson/self/agents/ExampleAgent.java
+++ self-java-sdk/src/test/java/com/ibm/watson/self/agents/ExampleAgent.java
@@ -1,3 +1,21 @@
+
+/**
+* Copyright 2016 IBM Corp. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
 package com.ibm.watson.self.agents;
 
 import java.util.UUID;
@@ -7,11 +25,10 @@ import org.apache.logging.log4j.Logger;
 
 import com.google.gson.JsonObject;
 import com.ibm.watson.self.blackboard.BlackBoard;
-import com.ibm.watson.self.blackboard.BlackBoardTest;
 import com.ibm.watson.self.blackboard.IBlackBoard;
 import com.ibm.watson.self.blackboard.IThing;
-import com.ibm.watson.self.blackboard.ThingEvent;
 import com.ibm.watson.self.blackboard.IThing.ThingEventType;
+import com.ibm.watson.self.blackboard.ThingEvent;
 
 public class ExampleAgent implements IAgent, IBlackBoard {
 
